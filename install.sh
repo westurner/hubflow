a38876bb84e8057bd18e0be4e4c8f309c12e7920
Michael Borsuk <mrb@wiredsoul.org>
2013-02-23 00:22:15 -0800

    Pass args on to gitflow-installer.sh
    Allow install, uninstall args to be passed through

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ if [ `id -u` != 0 ]; then
 	echo "*** error: must run as root (use sudo!)"
 	exit 1
 fi
+# Usage: [environment] installer.sh [install|uninstall]
+# Environment:
+#   INSTALL_PREFIX=$INSTALL_PREFIX" (default is /usr/local/bin)
+
 
 export REPO_NAME="`dirname $0`"
-bash $REPO_NAME/contrib/gitflow-installer.sh
+bash $REPO_NAME/contrib/gitflow-installer.sh "$@"
