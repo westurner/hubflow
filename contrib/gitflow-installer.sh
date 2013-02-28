2d79833afe94873772a5861f17ff7d13c555a0e4
Stuart Herbert <stuart@stuartherbert.com>
2013-02-28 16:46:21 +0000

    New: updated installer to install git-hf-push, git-hf-pull commands

--- contrib/gitflow-installer.sh
+++ contrib/gitflow-installer.sh
@@ -21,7 +21,7 @@ if [ -z "$REPO_HOME" ] ; then
 fi
 
 EXEC_FILES="git-hf"
-SCRIPT_FILES="git-hf-init git-hf-feature git-hf-hotfix git-hf-release git-hf-support git-hf-update git-hf-upgrade git-hf-version hubflow-common gitflow-shFlags"
+SCRIPT_FILES="git-hf-init git-hf-feature git-hf-hotfix git-hf-push git-hf-pull git-hf-release git-hf-support git-hf-update git-hf-upgrade git-hf-version hubflow-common gitflow-shFlags"
 SUBMODULE_FILE="gitflow-shFlags"
 
 echo "### gitflow / hubflow no-make installer ###"
